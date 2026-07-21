3b4239f9b0006e563844fb0f4eb302bf3605c464
Luqman Talha <luqmantalha2005@gmail.com>
2026-07-21 14:28:01 +0530

    Delete Firebase setup from firebase.ts
    Removed Firebase initialization and configuration.

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCYaA-AA_-yAOlqIlTwW05YoPs5XwJCITo",
-  authDomain: "jobshield-ai-2819d.firebaseapp.com",
-  projectId: "jobshield-ai-2819d",
-  storageBucket: "jobshield-ai-2819d.firebasestorage.app",
-  messagingSenderId: "992991370652",
-  appId: "1:992991370652:web:e5918b4f98678a6b027c59",
-  measurementId: "G-LTLMC73X5C"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
